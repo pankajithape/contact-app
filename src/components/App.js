e0ad3694d4ff9e0f03edfc668e8fc538bf55c174
Pankaj Ithape <44030634+pankajithape@users.noreply.github.com>
2022-03-02 16:34:14 +0530

    Made changes in app.js
    made some changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,7 @@
-import './App.css';
+import "./App.css";
 import React from "react";
 
-
 function App() {
-  return (
-    <div>contact - app</div>
-  );
+  return <div> contact - app</div>;
 }
 export default App;
